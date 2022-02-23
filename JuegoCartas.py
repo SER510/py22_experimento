0ac1a4f7c76b18f3b7bda62f19bcf7c1d6624d79
SER510 <SER510@users.noreply.github.com>
2022-02-23 17:37:33 +0100

    Generado modulo para barajas
    pos eso (para que veais que se puede abusar de escribir mal los comentarios)

--- JuegoCartas.py
+++ JuegoCartas.py
@@ -1,34 +1,6 @@
 
 
-from random import shuffle
-
-
-class Baraja(object):
-    """_summary_
-        Genera una baraja nueva.
-        Se ejecuta Baraja('europea')...
-        daria de resultado...
-
-    Args:
-        object (_type_): _description_
-    """
-    malapraxis=1 #variable global de clase suele ser mala idea
-
-    def __init__(self, tipoooo):
-        """_summary_
-
-        Args:
-            tipoooo (str): _description_
-        """
-        cartas_local = []
-        self.cartas = [1, 2, 3, 4, 5]   # lista
-        self.tipo = tipoooo             # tipo de baraja (española, francesa,...)
-
-    def mezclar(self):
-        shuffle(self.cartas)    # mezclamos el orden de las cartas
-    
-    def quitar_carta(self):
-        return self.cartas.pop()
+from Deck import Baraja
 
 class Jugador(object):
     def __init__(self, nombre='noname') -> None:
