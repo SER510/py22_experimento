0cabf6b4f497e5115da5004e070c4a426bb6cc24
SER510 <SER510@users.noreply.github.com>
2022-02-23 18:01:28 +0100

    movido la partida a un nuevo modulo

---
+++ GameType.py
@@ -0,0 +1,12 @@
+class Partida(object):
+    def __init__(self, lista_de_jugadores, baraja) -> None:
+        
+        self.jugadores = lista_de_jugadores
+        self.baraja = baraja
+        # self.cartas = baraja.cartas     # esto es lo que llamamos composicion
+    
+    def evaluar(self):
+        pass
+
+    def ronda(self):
+        pass
--- JuegoCartas.py
+++ JuegoCartas.py
@@ -4,23 +4,13 @@ from Deck import Baraja
 
 from Players import Jugador
 from Players import Gamer
+from GameType import Partida
 
 class Juego(object):
     pass
 
 
-class Partida(object):
-    def __init__(self, lista_de_jugadores, baraja) -> None:
-        
-        self.jugadores = lista_de_jugadores
-        self.baraja = baraja
-        # self.cartas = baraja.cartas     # esto es lo que llamamos composicion
-    
-    def evaluar(self):
-        pass
 
-    def ronda(self):
-        pass
 
 
 
