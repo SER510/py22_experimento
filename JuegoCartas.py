a9f44d93b37854e2747476d00dd328cfd3bb0464
Victoriano Fernández Tomás <victorianoft.upm@gmail.com>
2022-03-02 12:17:25 +0100

    Mano de cada jugador

--- JuegoCartas.py
+++ JuegoCartas.py
@@ -18,11 +18,22 @@ lucas = Gamer('Lucas')
 
 
 baraja1 = Baraja("española")
-
+baraja1.mezclar()
 
 partida1 = Partida( [gorka, jorge, antonio, lucas], baraja1 )
-baraja1.mezclar()
-jorge.coger_de_Baraja(baraja1,1)
-print('carta de jorge', jorge.mano)
+
+i = 0 #Ahora no sé hacerlo de otra forma y lo pongo así
+for i in [1, 2, 3]:
+    gorka.coger_de_Baraja(baraja1,1)
+    jorge.coger_de_Baraja(baraja1,1)
+    antonio.coger_de_Baraja(baraja1,1)
+    lucas.coger_de_Baraja(baraja1,1)
+    i = 1+i
+    #Fin del bucle
+    
+print('Cartas de gorka', gorka.mano)
+print('Cartas de jorge', jorge.mano)
+print('Cartas de jorge', antonio.mano)
+print('Cartas de jorge', lucas.mano)
 
 
