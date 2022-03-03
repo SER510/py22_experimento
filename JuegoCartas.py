b187f53eb56eebc38e33838f5f38fadcd35258e7
Victoriano Fernández Tomás <victorianoft.upm@gmail.com>
2022-03-03 11:23:45 +0100

    Jugadores y cartas
    He cambiado que puedas meter el número de jugadores por pantalla, su nombre y el número de cartas que van a coger

--- JuegoCartas.py
+++ JuegoCartas.py
@@ -6,34 +6,74 @@ from Players import Jugador
 from Players import Gamer
 from GameType import Partida
 
+import numpy as np
+
 class Juego(object):
     pass    
 
 
+print("Número de jugadores")
+num_jugadores = int(input())
+#print("Hay ", num_jugadores, " jugadores")
 
-gorka   = Jugador()
-jorge   = Jugador('Jorge')
-antonio = Gamer('Antonio')
-lucas = Gamer('Lucas')
+jugadores = np.empty(num_jugadores, dtype=object)
 
-baraja1 = Baraja("española")
+for i in range(num_jugadores):
+    print("Nombre de jugador ", i+1)
+    nombres = input()
+    jugadores[i] = Gamer(nombres)
+
+
+print("  ")
+
+
+baraja1 = Baraja("francesa")
 baraja1.mezclar()
 
-partida1 = Partida( [gorka, jorge, antonio, lucas], baraja1 )
 
-i = 0 #Ahora no sé hacerlo de otra forma y lo pongo así
-for i in [1, 2, 3]:
-    gorka.coger_de_Baraja(baraja1,1)
-    jorge.coger_de_Baraja(baraja1,1)
-    antonio.coger_de_Baraja(baraja1,1)
-    lucas.coger_de_Baraja(baraja1,1)
-    i = 1+i
-    #Fin del bucle
+partida1 = Partida( jugadores, baraja1)
 
-print('Cartas de gorka', gorka.mano)
-print('Cartas de jorge', jorge.mano)
-print('Cartas de antonio', antonio.mano)
-print('Cartas de lucas', lucas.mano)
+
+print("  ")
+
+
+print("Cartas que cogerá cada jugador")
+num_cartas = int(input())
+for i in range(num_cartas):
+    for j in range(num_jugadores):
+        jugadores[j].coger_de_Baraja(baraja1,1)
+
+
+
+for n in range(num_jugadores):
+    print('Cartas de Jugador', n+1, jugadores[n].mano)
+
+########################################################
+##### CÓDIGO QUE TENÍAMOS ANTES
+
+# gorka   = Jugador()
+# jorge   = Jugador('Jorge')
+# antonio = Gamer('Antonio')
+# lucas = Gamer('Lucas')
+
+# baraja1 = Baraja("española")
+# baraja1.mezclar()
+
+# partida1 = Partida( [gorka, jorge, antonio, lucas], baraja1 )
+
+# i = 0 #Ahora no sé hacerlo de otra forma y lo pongo así
+# for i in [1, 2, 3]:
+#     gorka.coger_de_Baraja(baraja1,1)
+#     jorge.coger_de_Baraja(baraja1,1)
+#     antonio.coger_de_Baraja(baraja1,1)
+#     lucas.coger_de_Baraja(baraja1,1)
+#     i = 1+i
+#     #Fin del bucle
+
+# print('Cartas de gorka', gorka.mano)
+# print('Cartas de jorge', jorge.mano)
+# print('Cartas de antonio', antonio.mano)
+# print('Cartas de lucas', lucas.mano)
 
 
 
